139cdc7ff5d466331c0980c3c9e683bbb0a97d8e
GabrielGarrido123 <gabriel.garrido@ug.uchile.cl>
2025-09-26 21:53:14 -0300

    update 0.2
    asdasdasd

--- sensores/sensores.ino
+++ sensores/sensores.ino
@@ -1,8 +1,47 @@
-void setup(){
+//definicion pines
+#define boton 2;
+#define buzzer 10;
+#define pinIR 11;
 
+bool debug = true; //para evitar que se esten mandando los outputs en consola cambiar a false
+int sensores[6] = {analogRead(A0),analogRead(A1),analogRead(A2),analogRead(A3),analogRead(A4),analogRead(A5)};
+
+void leer(){
+  digitalWrite(pinIR, HIGH); //"encendido" del IR
+  sensores[0] = analogRead(A0);
+  sensores[1] = analogRead(A1);
+  sensores[2] = analogRead(A2);
+  sensores[3] = analogRead(A3);
+  sensores[4] = analogRead(A4);
+  sensores[5] = analogRead(A5);
+  digitalWrite(pinIR,LOW);//"apagado" del IR
 }
 
-void loop(){
-
+int posRel(){
+  int ponderacion[6] = {-3,-2,-1,1,2,3};
+  int suma = 0;
+  for (int i = 0; i < 6; i++){
+    suma += sensores[i]*ponderacion[i];//esto claramente aun no da entre -255 y 255
+  }
   
+  int maxAct = 1023*3+1023*2+1023;
+  
+  int sumaMap = map(suma,-maxAct,maxAct,-255,255);//ahora si da la posicion pedida :D
+
+  if (sumaMap == 0){
+    return 0;
+  }
+  return sumaMap;
+}
+
+void setup()
+{
+  pinMode(11, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int p = posRel();
+  Serial.println(p);
 }
