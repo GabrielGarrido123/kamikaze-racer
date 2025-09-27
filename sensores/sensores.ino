bffae540139a6bfbb0eb323abc1a883f45deb743
GabrielGarrido123 <gabriel.garrido@ug.uchile.cl>
2025-09-26 22:34:55 -0300

    update 0.4

--- sensores/sensores.ino
+++ sensores/sensores.ino
@@ -4,56 +4,52 @@
 #define pinIR 11
 
 bool debug = true; //para evitar que se esten mandando los outputs en consola cambiar a false
-int sensores[] = {0,0,0,0,0,0};
+int s[] = {0,0,0,0,0,0};
+int pos = 0;
+int lastPos = 0;
+
+void setup()
+{
+  pinMode(pinIR, OUTPUT);
+  Serial.begin(9600);
+}
 
 void leer(){
   digitalWrite(pinIR, HIGH); //"encendido" del IR
-  sensores[0] = analogRead(A0);
-  sensores[1] = analogRead(A1);
-  sensores[2] = analogRead(A2);
-  sensores[3] = analogRead(A3);
-  sensores[4] = analogRead(A4);
-  sensores[5] = analogRead(A5);
+  s[0] = analogRead(A0);
+  s[1] = analogRead(A1);
+  s[2] = analogRead(A2);
+  s[3] = analogRead(A3);
+  s[4] = analogRead(A4);
+  s[5] = analogRead(A5);
   digitalWrite(pinIR,LOW);//"apagado" del IR
 }
 
 int posRel(){
   leer();
-  float sumaPonderada = -2.5*sensores[0]-1.5*sensores[1]-0.5*sensores[2]+0.5*sensores[3]+1.5*sensores[4]+2.5*sensores[5];
-  float suma = sensores[0]+sensores[1]+sensores[2]+sensores[3]+sensores[4]+sensores[5];
+  float sumaPonderada = -2.5*s[0]-1.5*s[1]-0.5*s[2]+0.5*s[3]+1.5*s[4]+2.5*s[5];
+  float suma = s[0]+s[1]+s[2]+s[3]+s[4]+s[5];
   bool enLinea = false;
 
-  if (suma > 1000){online = true};
+  if (suma > 1000){
+    enLinea = true;
+    }
 
-  if (online){
-    pos = 
+  if (enLinea){
+    pos = int(100.0 * sumaPonderada / suma);
+  } else { //trata de volver a la linea
+    if (lastPos < 0) {
+      pos = -255;
+    } else if (lastPos >= 0) {
+      pos = 255;
+    }
   }
-
-
-  int ponderacion[6] = {-3,-2,-1,1,2,3};
-  int suma = 0;
-  for (int i = 0; i < 6; i++){
-    suma += sensores[i]*ponderacion[i];//esto claramente aun no da entre -255 y 255
-  }
-  
-  int maxAct = 1023*3+1023*2+1023;
-  
-  int sumaMap = map(suma,-maxAct,maxAct,-255,255);//ahora si da la posicion pedida :D
-
-  if (sumaMap == 0){
-    return 0;
-  }
-  return sumaMap;
-}
-
-void setup()
-{
-  pinMode(11, OUTPUT);
-  Serial.begin(9600);
+  lastPos = pos;
+  return pos;
 }
 
 void loop()
 {
   int p = posRel();
-  Serial.println(p);
+  if (debug){Serial.println(p);}
 }
