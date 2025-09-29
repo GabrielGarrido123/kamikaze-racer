9b22f1f6b4e28b5823b9e659bf0347f0c82edbd1
GabrielGarrido123 <gabriel.garrido@ug.uchile.cl>
2025-09-28 23:54:28 -0300

    update 0.5.3
    añadi las cosas al serial

--- prueba_motores/prueba_motores.ino
+++ prueba_motores/prueba_motores.ino
@@ -14,33 +14,45 @@ void setup() {
   motores(0, 0);
   delay(500);
 
+  Serial.println("Presiona el boton para que ambos motores avancen.");
   esperarBoton(); //presionar boton para que los motores avancen
   motores(50, 50);
   delay(1000);
 
+  Serial.println("Presiona el boton para que ambos motores retrocedan.");
   esperarBoton(); //presionar boton para que los motores retrocedan
   motores(-50, -50);
   delay(1000);
 
+  Serial.println("Presiona el boton para que ambos motores se detengan.");
   esperarBoton(); //presionar el boton para que los motores se detengan
   motores(0, 0);
   delay(100);
 
+  Serial.println("Presiona el boton para que el motor izquerdo avance.");
   esperarBoton(); //presionar el boton para que el motor izquierdo avance
   motores(50, 0)
   delay(1000);
 
+  Serial.println("Presiona el boton para que el motor izquerdo retroceda.");
   esperarBoton(); //presionar el boton para que el motor izquierdo retroceda
   motores(-50, 0)
   delay(1000);
 
+  Serial.println("Presiona el boton para que el motor derecho avance.");
   esperarBoton(); //presionar el boton para que el motor derecho avance
   motores(0, 50)
   delay(1000);
 
+  Serial.println("Presiona el boton para que el motor derecho retroceda.");
   esperarBoton(); //presionar el boton para que el motor derecho retroceda
   motores(0,-50)
   delay(1000);
+
+  Serial.println("Presiona el boton para que ambos motores se detengan y concluir el testeo de motores.");
+  esperarBoton(); //presionar el boton para que los motores se detengan
+  motores(0, 0);
+  delay(100);
 }
 
 
