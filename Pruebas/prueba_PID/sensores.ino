351b08da5b519ad3e7f0bc248bf88b2a5f9f8b94
GabrielGarrido123 <gabriel.garrido@ug.uchile.cl>
2025-11-03 17:54:49 -0300

    0.6.02

--- Pruebas/prueba_PID/sensores.ino
+++ Pruebas/prueba_PID/sensores.ino
@@ -4,6 +4,8 @@
 
 int pos = 0;
 int lastPos = 0;
+int v_s_max[] = {0,0,0,0,0,0};
+int v_s_min[] = {1023,1023,1023,1023,1023,1023};
 
 void sensores_setup()
 {
@@ -49,3 +51,63 @@ int posRel(){
   return pos;
 }
 
+void calibracion() {
+  digitalWrite(pinIR, HIGH);
+
+  int v_s[6];
+
+  for (int j = 0; j < 100; j++) {
+    delay(30);
+    v_s[0] = analogRead(A6);
+    v_s[1] = analogRead(A5);
+    v_s[2] = analogRead(A4);
+    v_s[3] = analogRead(A3);
+    v_s[4] = analogRead(A2);
+    v_s[5] = analogRead(A1);
+
+    for (int i = 0; i < 6; i++) {
+
+      Serial.print(v_s[i]);
+      Serial.print("\t");
+    }
+    Serial.println();
+
+    for (int i = 0; i < 6; i++) {
+      if (v_s[i] < v_s_min[i]) {
+        v_s_min[i] = v_s[i];
+      }
+    }
+
+
+    for (int i = 0; i < 6; i++) {
+      if (v_s[i] > v_s_max[i]) {
+        v_s_max[i] = v_s[i];
+      }
+    }
+  }
+
+  bip();
+  bip();
+
+  Serial.println();
+  Serial.println();
+
+  Serial.print("Minimos");
+  Serial.print("\t");
+
+  for (int i = 0; i < 6; i++) {
+    Serial.print(v_s_min[i]);
+    Serial.print("\t");
+  }
+  Serial.println();
+
+  Serial.print("Maximos");
+  Serial.print("\t");
+
+  for (int i = 0; i < 6; i++) {
+    Serial.print(v_s_max[i]);
+    Serial.print("\t");
+  }
+  Serial.println();
+  digitalWrite(pinIR, LOW);
+}
